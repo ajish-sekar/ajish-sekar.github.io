c12099e18499c26fce84e956fc386e0f41af0a97
Ajish Sekar <ajish.sekar@gmail.com>
2021-09-27 12:47:01 -0400

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    // intitialize side nave
+    $('.sidenav').sidenav();
+    // open side nav in mobile
+    $('.sidenav').sidenav('open');
+    
+    // initialize tooltip
+    $('.tooltipped').tooltip();
+    
+    // intitialize scrollspy
+    $('.scrollspy').scrollSpy();
+  });
